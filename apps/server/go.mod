c6fc70ef10e81e2a5af9dabebc98330932aa50b2
dev <luke@coldbrewstudios.co.uk>
2023-04-25 21:45:59 +0100

    feat: created basic apps for client and server

--- apps/server/go.mod
+++ apps/server/go.mod
@@ -1,3 +1,5 @@
 module github.com/codewith-luke/disclone
 
 go 1.19
+
+require github.com/go-chi/chi/v5 v5.0.8 // indirect
--- apps/server/main.go
+++ apps/server/main.go
@@ -1,7 +1,17 @@
 package main
 
-import("fmt")
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome"))
+	})
+
+	http.ListenAndServe(":3000", r)
 }
