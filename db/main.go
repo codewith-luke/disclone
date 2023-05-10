c378bc04534987bac4ce79a2ffad412c7ddb5178
dev <luke@coldbrewstudios.co.uk>
2023-05-10 20:29:39 +0100

    feat: updated moon tasks

--- db/main.go
+++ db/main.go
@@ -8,15 +8,22 @@ import (
 	"github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 	_ "github.com/lib/pq"
+	"os"
 )
 
 func main() {
 	upPtr := flag.Bool("up", false, "a bool")
 	downPtr := flag.Bool("down", false, "a bool")
-
 	flag.Parse()
 
-	db, err := sql.Open("postgres", "postgres://postgres:postgres@localhost:5432/postgres?sslmode=disable")
+	user := os.Getenv("DB_USER")
+	pwd := os.Getenv("DB_PWD")
+	host := os.Getenv("DB_HOST")
+	database := os.Getenv("DB_DATABASE")
+	port := os.Getenv("DB_PORT")
+
+	con := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", user, pwd, host, port, database)
+	db, err := sql.Open("postgres", con)
 
 	if err != nil {
 		panic(err)
@@ -36,6 +43,14 @@ func main() {
 
 	if err != nil {
 		panic(err)
+	}
+
+	if *upPtr && *downPtr {
+		panic("Cannot specify both up and down")
+	}
+
+	if !*upPtr && !*downPtr {
+		panic("Must specify either up or down")
 	}
 
 	if *upPtr {
