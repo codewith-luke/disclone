745a6148d4b5382acb76dffc6bcbdba0d791cfe4
Luke Babich <luke@coldbrewstudios.co.uk>
2023-08-19 18:23:30 +0100

    fix: corrected bug with combo box instances

--- apps/client/src/lib/FriendProfile.svelte
+++ apps/client/src/lib/FriendProfile.svelte
@@ -7,18 +7,40 @@
     <button use:popup={popupCombobox}>
         <MoreVertical class="opacity-0 group-hover:opacity-100"/>
     </button>
+
+    <div class="card w-48 shadow-xl py-2 fixed z-10 text-surface-400" data-popup={`popupComboBox-${index}`}>
+        <ListBox class="relative z-10" rounded="rounded-none">
+            <ListBoxItem bind:group={comboboxValue} name="medium" value="remove">
+                <div class="flex items-center gap-x-2">
+                    <MinusCircle/>
+                    <span>
+                        Remove Friend
+                    </span>
+                </div>
+            </ListBoxItem>
+            <ListBoxItem bind:group={comboboxValue} name="medium" value="add_to_chat">
+                <div class="flex items-center gap-x-2">
+                    <PlusCircle/>
+                    <span>
+                        Add to chat
+                    </span>
+                </div>
+            </ListBoxItem>
+        </ListBox>
+        <div class="arrow bg-surface-100-800-token"/>
+    </div>
 </div>
 
 <script lang="ts">
     import UserProfileAction from "./UserProfileAction.svelte";
-    import {MoreVertical} from "lucide-svelte";
-    import {type PopupSettings, popup} from "@skeletonlabs/skeleton";
+    import {MinusCircle, MoreVertical, PlusCircle} from "lucide-svelte";
+    import {type PopupSettings, popup, ListBoxItem, ListBox} from "@skeletonlabs/skeleton";
 
     export let index;
 
     const popupCombobox: PopupSettings = {
         event: 'focus-click',
-        target: 'popupCombobox',
+        target: `popupComboBox-${index}`,
         placement: 'right-start',
         closeQuery: '.listbox-item'
     };
--- apps/client/src/lib/FriendsList.svelte
+++ apps/client/src/lib/FriendsList.svelte
@@ -1,25 +1,4 @@
 <div class="flex flex-col gap-y-4 mt-4">
-    <div class="card w-48 shadow-xl py-2 fixed z-10 text-surface-400" data-popup="popupCombobox">
-        <ListBox class="relative z-10" rounded="rounded-none">
-            <ListBoxItem bind:group={comboboxValue} name="medium" value="remove">
-                <div class="flex items-center gap-x-2">
-                    <MinusCircle/>
-                    <span>
-                        Remove Friend
-                    </span>
-                </div>
-            </ListBoxItem>
-            <ListBoxItem bind:group={comboboxValue} name="medium" value="add_to_chat">
-                <div class="flex items-center gap-x-2">
-                    <PlusCircle/>
-                    <span>
-                        Add to chat
-                    </span>
-                </div>
-            </ListBoxItem>
-        </ListBox>
-        <div class="arrow bg-surface-100-800-token"/>
-    </div>
 
     {#each Array(users) as _, idx}
         <FriendProfileAction index={idx}/>
