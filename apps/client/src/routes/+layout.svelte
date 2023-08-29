af1ecf498bf1fa113e30387ac6890b5b2a02302a
Luke Babich <luke@coldbrewstudios.co.uk>
2023-08-29 20:26:47 +0100

    (wip)feat: implementing API spec

--- apps/client/src/routes/+layout.svelte
+++ apps/client/src/routes/+layout.svelte
@@ -4,7 +4,6 @@
     import './styles.css';
     import {initializeStores, Modal, storePopup} from '@skeletonlabs/skeleton';
     import {arrow, autoUpdate, computePosition, flip, offset, shift} from '@floating-ui/dom';
-    import ChatArea from "$lib/ChatArea.svelte";
 
     storePopup.set({computePosition, autoUpdate, offset, shift, flip, arrow});
     initializeStores();
@@ -16,7 +15,7 @@
     <div class="flex flex-1 overflow-hidden">
         <SideNav/>
         <div class="w-full">
-           <ChatArea/>
+            <slot/>
         </div>
     </div>
 </div>
